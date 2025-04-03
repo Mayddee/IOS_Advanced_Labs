ce509ca89f065d36b9dc628596ccfb237779491e
Mayddee <a.madikosh2004@gmail.com>
2025-04-04 00:08:24 +0500

    lab

--- lab5/FiveImages/FiveImages/View/GridView.swift
+++ lab5/FiveImages/FiveImages/View/GridView.swift
@@ -11,12 +11,11 @@ struct GridView: View {
     let images: [ImageModel]
         let spacing: CGFloat
         
-        // Dynamically calculate the number of columns based on screen size
         private var columns: Int {
             let screenWidth = UIScreen.main.bounds.width
-            if screenWidth < 375 { // Small screen, 2 columns
+            if screenWidth < 375 {
                 return 2
-            } else { // Larger screen, 3 columns
+            } else {
                 return 3
             }
         }
@@ -28,16 +27,16 @@ struct GridView: View {
                     
                     LazyVGrid(
                         columns: Array(repeating: GridItem(.flexible(), spacing: spacing), count: columns),
-                        spacing: spacing // Ensure equal spacing between rows
+                        spacing: spacing // to ensure equal spacing between rows
                     ) {
                         ForEach(images) { model in
                             model.image
                                 .resizable()
                                 .scaledToFill()
-                                .frame(width: width, height: model.height) // Dynamically assign height
+                                .frame(width: width, height: model.height) 
                                 .clipShape(RoundedRectangle(cornerRadius: 12))
                                 .shadow(color: .gray.opacity(0.2), radius: 6, x: 0, y: 4)
-                                .padding(.bottom, spacing) // Padding between images to ensure equal vertical spacing
+                                .padding(.bottom, spacing)
                         }
                     }
                     .padding(.horizontal, spacing)
